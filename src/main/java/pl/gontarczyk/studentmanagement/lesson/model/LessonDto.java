6842b67de21b8408fb19742c8dd18f4b0e3eb091
Konrad <konrad326@opoczta.pl>
2023-06-26 04:15:56 +0200

    lesson update

--- src/main/java/pl/gontarczyk/studentmanagement/lesson/model/LessonDto.java
+++ src/main/java/pl/gontarczyk/studentmanagement/lesson/model/LessonDto.java
@@ -1,19 +1,22 @@
 package pl.gontarczyk.studentmanagement.lesson.model;
 
 import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.Min;
-import lombok.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
-@NoArgsConstructor
 @Getter
 @Setter
 @Builder
 public class LessonDto {
 
+    @NotNull(message = "You must give date!")
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     @Future(message = "You can't save lesson in the past!")
     private LocalDateTime dateTime;
